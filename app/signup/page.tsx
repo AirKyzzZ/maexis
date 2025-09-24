431125396a8c03da0906e37622baac3cb2d6d4ad
AirKyzzZ <airkyzzz.jiji@gmail.com>
2025-09-24 20:26:15 +0200

    fix: build vercel

---
+++ app/signup/page.tsx
@@ -0,0 +1,10 @@
+export default function SignupPage() {
+  return (
+    <div className="min-h-screen">
+      {/* TODO: Implement signup content */}
+      <h1 className="sr-only">Signup</h1>
+    </div>
+  )
+}
+
+
